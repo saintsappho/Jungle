87e373b70211f13fa409c2d5f6746f52f3bcefde
saintsappho <robinrosefleur@gmail.com>
2024-01-25 14:53:31 -0700

    Admin Auth and dashboard customization.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authenticate_admin!
   def show
+    @category = Category
+    @products = Product
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,4 +29,11 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
+
+  def authenticate_admin!
+    return if Rails.env.development?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USERNAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 end
