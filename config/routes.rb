f93dd3e9e28a682337c75e593c49b4f7c0688136
saintsappho <robinrosefleur@gmail.com>
2024-01-25 22:42:23 -0700

    new views for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get 'about', to: 'static#about'
+  
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-  get 'about', to: 'static#about'
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
 
   resources :orders, only: [:create, :show]
-
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
