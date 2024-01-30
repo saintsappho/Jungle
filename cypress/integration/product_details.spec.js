f15bf0990db247bf26f8bbd723c58c6d8633fb4d
saintsappho <robinrosefleur@gmail.com>
2024-01-30 14:25:05 -0700

    product testing and cart confirmation

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -5,10 +5,16 @@ it("Visits the homepage", () => {
 it("There are products on the page", () => {
   cy.get(".products article").should("be.visible");
 });
-xit("There is 2 products on the page", () => {
+it("There is 2 products on the page", () => {
   cy.get(".products article").should("have.length", 2);
 });
+it("Displays sold out badge when inventory is 0", () => {
+  cy.get(".soldout").should("be.visible")
+})
+it("navigates to product information", () => {
+  cy.get(".products article").first().click()
+})
 it("adds product to cart, and cart counter increases", () => {
-  cy.get(".btn").first().click({force: true})
+  cy.get(".btn").click()
   cy.get(".nav-link").contains("My Cart (1)")
 })
